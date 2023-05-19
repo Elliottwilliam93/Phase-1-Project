6393a351cc260bd0a24fc9dd4b5a2f357e5fc719
williamelliott <ironmaiden229@hotmail.com>
2023-05-19 13:11:07 -0400

    Added a changing background and added new challenge rating of monster on card

---
+++ main/index.js
@@ -0,0 +1,24 @@
+const backgroundImages = [
+    'https://wallpaperaccess.com/full/117898.jpg',
+    'https://wallpaperaccess.com/full/2077763.jpg',
+    'https://e1.pxfuel.com/desktop-wallpaper/875/598/desktop-wallpaper-fantasy-art-artwork-pathfinder-and-mobile-backgrounds-pathfinder-kingmaker.jpg',
+    'https://wallpaper-house.com/data/out/9/wallpaper2you_294923.jpg',
+    'https://e0.pxfuel.com/wallpapers/145/280/desktop-wallpaper-dota-1-digital.jpg',
+  ];
+  
+  let backgroundImageIndex = 0;
+  
+  function changeBackgroundImage() {
+    const currentImage = backgroundImages[backgroundImageIndex];
+    document.body.style.backgroundImage = `url(${currentImage})`;
+    document.body.style.backgroundSize = 'cover'; // set background size to cover
+    backgroundImageIndex = (backgroundImageIndex + 1) % backgroundImages.length;
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    changeBackgroundImage(); // set initial background image
+    setInterval(changeBackgroundImage, 10000); // change background image every 10 seconds
+  
+    // CSS transitions for fade effect
+    document.body.style.transition = 'background-image 1s ease-in-out';
+  });
