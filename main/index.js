ce132922cfab9100dcd620a98c997b075ee90052
williamelliott <ironmaiden229@hotmail.com>
2023-05-31 16:44:49 -0400

    Moved all JS over to the index.js file and made sure it functions properly.

--- main/index.js
+++ main/index.js
@@ -21,3 +21,86 @@ document.addEventListener('DOMContentLoaded', function() {
   
   document.body.style.transition = 'background-image 2s ease-in-out';
 });
+
+let storedValue;
+      const cardTemplate = document.getElementById("card-template");
+
+      function storeInputValue() {
+        let input = document.getElementById("myInput");
+
+        storedValue = input.value;
+        useStoredValue();
+      }
+      
+      function convertSpacesToDashes(str) {
+        return str.replace(/\s+/g, "-");
+      }
+
+      function useStoredValue() {
+        let lowerValue = storedValue.toLowerCase();
+        let newValue = convertSpacesToDashes(lowerValue);
+        let mydata = fetch(`https://api.open5e.com/monsters/${newValue}`)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            const element = data;
+            console.log(element);
+            let crSRC;
+
+            if (element.challenge_rating <= 5 ) {
+              crSRC = '<img src="resources/diablo3-difficulty-levels (2).png" style="height: 150px; width: 150px; border-radius: 25px;">'
+            }
+            else if (element.challenge_rating <= 10 ) {
+              crSRC = '<img src="resources/diablo3-difficulty-levels.png" style="height: 150px; width: 150px; border-radius: 25px;">'
+            }
+            else if (element.challenge_rating <= 15 ) {
+              crSRC = '<img src="resources/diablo3-difficulty-levels (3).png" style="height: 150px; width: 150px; border-radius: 25px;">'
+            }
+            else {
+              crSRC = '<img src="resources/diablo3-difficulty-levels (4).png" style="height: 150px; width: 150px; border-radius: 25px;">'
+            }
+            if(element.detail === "Not found.") {
+              alert("Monster Not Found!")
+            }else {
+            const newCard = cardTemplate.content.cloneNode(true);
+            newCard.querySelector(".card-title").textContent = element.name;
+            newCard.querySelector(".card-size").textContent = element.size;
+            newCard.querySelector(".card-type").textContent = element.type;
+            newCard.querySelector(".cr-img").innerHTML = crSRC
+            newCard.querySelector(".card-alignment").textContent = element.alignment;
+            newCard.querySelector(".card-challenge_rating").textContent = element.challenge_rating
+            
+            const likeButton = newCard.querySelector(".like-button");
+            let likes = 0;
+            likeButton.addEventListener("click", () => {
+              likes++;
+              likeButton.textContent = `Like (${likes})`;
+            });
+            
+            const demoSection = document.getElementById("demo");
+            demoSection.appendChild(newCard);
+          }
+            storedValue = "";
+            mydata = "";
+          })
+          .catch((error) => {           
+          });
+      }
+      const form = document.querySelector('.comment-section form');
+      const commentHistory = document.querySelector('.comment-history');
+
+      form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const comment = form.comment.value.trim();
+        if (!comment) return;
+
+        const commentElement = document.createElement('div');
+        commentElement.classList.add('comment');
+        commentElement.innerHTML = `
+          <p>${comment}</p>
+          <span>${new Date().toLocaleString()}</span>
+          `;
+        commentHistory.appendChild(commentElement);
+        form.reset();
+      });
